2648d460ff515cc9ec8a29bd814db125fc44c2d6
cruzer3008 <31630057+cruzer3008@users.noreply.github.com>
2018-07-13 14:31:56 +0530

    Added a new "Why SmartBulbs" section

--- Interns/js/homePage.js
+++ Interns/js/homePage.js
@@ -27,3 +27,12 @@ $(document).ready( function() {
 	});
 });
 
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
+
