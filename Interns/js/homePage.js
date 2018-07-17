1d2ccb4ebf01b39ed432437b1ef6ec01da620180
maitri0411 <maitribaria96@gmail.com>
2018-07-17 12:06:20 +0530

    Added background colors to different sections

--- Interns/js/homePage.js
+++ Interns/js/homePage.js
@@ -1,18 +1,15 @@
-$('.flip').hover(function(){
-        $(this).find('.card').toggleClass('flipped');
 
- });
 
 $(document).ready( function() {
     $('#myCarousel').carousel({
     	interval:   4000
 	});
-	
+
 	var clickEvent = false;
 	$('#myCarousel').on('click', '.nav a', function() {
 			clickEvent = true;
 			$('.nav li').removeClass('active');
-			$(this).parent().addClass('active');		
+			$(this).parent().addClass('active');
 	}).on('slid.bs.carousel', function(e) {
 		if(!clickEvent) {
 			var count = $('.nav').children().length -1;
@@ -20,7 +17,7 @@ $(document).ready( function() {
 			current.removeClass('active').next().addClass('active');
 			var id = parseInt(current.data('slide-to'));
 			if(count == id) {
-				$('.nav li').first().addClass('active');	
+				$('.nav li').first().addClass('active');
 			}
 		}
 		clickEvent = false;
@@ -39,19 +36,19 @@ $(document).ready(function() {
 // Scroll to specific values
 // scrollTo is the same
 window.scroll({
-  top: 2500, 
-  left: 0, 
-  behavior: 'smooth' 
+  top: 2500,
+  left: 0,
+  behavior: 'smooth'
 });
 
-// Scroll certain amounts from current position 
-window.scrollBy({ 
+// Scroll certain amounts from current position
+window.scrollBy({
   top: 100, // could be negative value
-  left: 0, 
-  behavior: 'smooth' 
+  left: 0,
+  behavior: 'smooth'
 });
 
 // Scroll to a certain element
-document.querySelector('.hello').scrollIntoView({ 
-  behavior: 'smooth' 
+document.querySelector('.hello').scrollIntoView({
+  behavior: 'smooth'
 });
